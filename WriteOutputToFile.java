3e93c11d1ec4043714da83319d15986aea86afab
smduvvuri <shreemaduvvuri@gmail.com>
2022-05-08 19:42:14 -0700

    changes

--- WriteOutputToFile.java
+++ WriteOutputToFile.java
@@ -2,26 +2,6 @@ import java.io.*;
 
 public class WriteOutputToFile {
 
-    public void writeToCSVFile(String item, int quantity, int price){
-
-
-        FileWriter pw=null;
-        try {
-
-            pw = new FileWriter("finaloutput.csv", true);
-            pw.write(item);
-            pw.write(",");
-            pw.write(Integer.toString(quantity));
-            pw.write(",");
-            pw.write(Integer.toString(price));
-            pw.write("\n");
-            pw.close();
-        }
-        catch (IOException e) {
-            throw new RuntimeException(e);
-        }
-    }
-
     public void writeToTextFile(String message, String item){
         PrintWriter out = null;
         try {
